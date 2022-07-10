2fe5a6be1924c87004488c85465fcc57d030ad7a
Renan Valladão <renancvalladao@gmail.com>
2022-07-10 20:35:10 -0300

    Removendo atributos não usados

--- TreeNode.py
+++ TreeNode.py
@@ -2,8 +2,6 @@ class TreeNode:
     def __init__(self):
         self.children = []
         self.sibling = None
-        self.lineno = None
-        self.kind = None
         self.attr = None
         self.type = None
 
